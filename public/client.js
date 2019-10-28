90003f3013524b4e0373253c75301923f95a9125
yourtechsis <aretee64@gmail.com>
2019-10-28 17:18:10 -0600

    text and video chat

--- public/client.js
+++ public/client.js
@@ -1,142 +1,153 @@
+// SEE SOCKET.IO, PEER & PEERJs DOCUMENTATION
+
 const socket = io();
 
+// variables for DOM elems
 const message = document.getElementById('message'),
-      handle = document.getElementById('handle'),
-      output = document.getElementById('output'),
-      typing = document.getElementById('typing'),
-      button = document.getElementById('button');
+    handle = document.getElementById('handle'),
+    output = document.getElementById('output'),
+    typing = document.getElementById('typing'),
+    button = document.getElementById('button');
 
-   //   send typing message
-   message.addEventListener('keypress', () =>{
+//   send typing message to server
+message.addEventListener('keypress', () => {
     socket.emit('userTyping', handle.value)
 })
 
-    
-// send messages to clients
-      button.addEventListener('click', () =>{
-          socket.emit('userMessage', {
-              handle: handle.value,
-              message: message.value
-          })
-          document.getElementById('message').value="";
-      })
 
- 
+// button click send messages to server, then gets sent to clients 
+button.addEventListener('click', () => {
+    socket.emit('userMessage', {
+        handle: handle.value,
+        message: message.value
+    })
+    document.getElementById('message').value = "";
+})
 
 
-    //   listen for events from the server
-     
-    socket.on("userMessage", (data) => {
-          typing.innerHTML ="";
-           output.innerHTML += '<p> <strong>' + data.handle + ': </strong>' + data.message + '</p>'
-      })
 
-      socket.on('userTyping', (data)=>{
-          typing.innerHTML = '<p><em>' + data + ' is typing... </em></p>'
-      })
 
-      
+//   listen for events coming from the server
 
-      /* video chat */
+socket.on("userMessage", (data) => {
+    typing.innerHTML = "";
+    output.innerHTML += '<p> <strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
 
-    //   get the local video and display it with permission
-    function getLVideo(callbacks) {
+socket.on('userTyping', (data) => {
+    typing.innerHTML = '<p><em>' + data + ' is typing... </em></p>'
+})
+
+/* =======================================
+
+PART II VIDEO CHAT  
+
+==========================================*/
+
+
+//   get the local video and display it with permission
+//mozilla.org
+function getLVideo(callbacks) {
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    var constraints ={
-        audio:true,
-        video:true
+    var constraints = {
+        audio: true,
+        video: true
     }
     navigator.getUserMedia(constraints, callbacks.success, callbacks.error)
 
-    }
-    function recStream(stream, elemid) {
-        var video = document.getElementById(elemid);
+}
 
-        video.srcObject = stream;
+// diplay our stream if we have permission of the media
+function recStream(stream, elemid) {
+    var video = document.getElementById(elemid);
 
-        window.peer_stream = stream;
-    }
-    getLVideo({
-        success: function(stream){
-            window.localstream = stream;
-            recStream(stream, 'lVideo');
-        },
-        error: function(err){
-            alert("cannot access your camera");
-            console.log(err);
-        }
-    })
-    
-    var conn;
-    var peer_id;
-    
-    // create a peer connection with peer obj
-    var peer = new Peer({key: 'lwjd5qra8257b9'});
+    video.srcObject = stream;
 
-    // display the peer id on the DOM
-    peer.on('open', function(){
-        document.getElementById("displayId").innerHTML = peer.id
-    })
-
-    peer.on('connection', function(connection){
-        conn = connection;
-        peer_id = connection.peer
-
-        document.getElementById('connId').value = peer_id;
-    });
-    peer.on('error', function(err){
-        alert("an error has happened:" + err);
+    window.peer_stream = stream;
+}
+// function takes in a obj for the promise of using the media or not
+getLVideo({
+    success: function (stream) {
+        window.localstream = stream;
+        recStream(stream, 'lVideo');
+    },
+    error: function (err) {
+        alert("cannot access your camera");
         console.log(err);
-    })
-    // onclick with the connection butt = expose ice info
-    document.getElementById('conn_button').addEventListener('click', function(){
-        peer_id = document.getElementById("connId").value;
+    }
+})
 
-        if(peer_id){
-            conn = peer.connect(peer_id)
-        }else{
-            alert("enter an id");
-            return false;
-        }
-    })
+// global variables for assign local and remote
+var conn;
+var peer_id;
 
-    // call on click (offer and answer is exchanged)
-    peer.on('call', function(call){
+// create a peer connection with peer obj or create you own using peer server docs
+var peer = new Peer({ key: 'lwjd5qra8257b9' });
 
-        var acceptCall = confirm("Do you want to answer this call?");
+// display the peer id on the DOM 
+peer.on('open', function () {
+    document.getElementById("displayId").innerHTML = peer.id
+})
 
-        if(acceptCall){
-            call.answer(window.localstream);
+// when a client connects to another connected client
+peer.on('connection', function (connection) {
+    conn = connection;
+    peer_id = connection.peer
 
-            call.on('stream', function(stream){
+    document.getElementById('connId').value = peer_id;
+});
 
-                window.peer_stream = stream;
+// 
+peer.on('error', function (err) {
+    alert("an error has happened:" + err);
+    console.log(err);
+})
+// onclick connection button 
+document.getElementById('conn_button').addEventListener('click', function () {
+    peer_id = document.getElementById("connId").value;
+    // if there is a peer id, use global var to connect with current peerid
+    if (peer_id) {
+        conn = peer.connect(peer_id)
+    } else {
+        alert("enter an id");
+        return false;
+    }
+})
+// call when call button is clicked
+peer.on('call', function (call) {
 
-                recStream(stream, 'rVideo')
-            });
+    // this prompt can act funny if the browser tab is not ACTIVE it can return false
+    var acceptCall = confirm("Do you want to answer this call?");
+    // if accpt, exchange across the browser
+    if (acceptCall) {
+        call.answer(window.localstream);
 
-            call.on('close', function(){
-                alert('The call has behind');
-            })
-        } else {
-            console.log("call denied")
-        }
-    });
+        call.on('stream', function (stream) {
 
-    // ask to call 
-    document.getElementById('call_button').addEventListener('click', function(){
-        console.log("calling a peer: "+ peer_id);
-        console.log(peer);
-
-        var call = peer.call(peer_id, window.localstream);
-
-        call.on('stream', function(stream){
             window.peer_stream = stream;
 
-            recStream(stream, 'rVideo');
+            recStream(stream, 'rVideo')
+        });
+        // display alert if call is denien
+        call.on('close', function () {
+            alert('The call has been denind');
         })
-    })
-   
-    // accpet the call
+    } else {
+        console.log("call denied")
+    }
+});
 
-    // display the remote video and local video on the clients
+// fires to the call event to initiate a call 
+document.getElementById('call_button').addEventListener('click', function () {
+    console.log("calling a peer: " + peer_id);
+    console.log(peer);
+
+    // using our peer id to call the other then to display each others media
+    var call = peer.call(peer_id, window.localstream);
+
+    call.on('stream', function (stream) {
+        window.peer_stream = stream;
+
+        recStream(stream, 'rVideo');
+    })
+}); 
--- server.js
+++ server.js
@@ -4,24 +4,28 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
+// port setup and listening 
 http.listen(PORT, () => {
     console.log("listening on port " + PORT)
 });
 
+// serving our index.html 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
+// allows files in this folder to be served all the time
 app.use(express.static('public'));
 
-
-io.on('connection', function(socket){
+// init socket 
+io.on('connection', function (socket) {
     console.log("client is connected " + socket.id)
-
+    // receive the event, then send data to clients
     socket.on('userMessage', (data) => {
         io.sockets.emit("userMessage", data)
     })
-    socket.on('userTyping', (data) =>{
+    // receive the typing event, send out to clients
+    socket.on('userTyping', (data) => {
         socket.broadcast.emit('userTyping', data)
-    
+
     });
 })
